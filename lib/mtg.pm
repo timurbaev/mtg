5c279d428abb1b3b01445cdbbeb162e349064e08
timur <timur@timur>
2017-07-08 12:26:10 +0300

    Added constants and modules

--- lib/mtg.pm
+++ lib/mtg.pm
@@ -1,7 +1,17 @@
 package mtg;
 use Dancer2;
+use Dancer2::Plugin::Database;
+use Store;
 
 our $VERSION = '0.1';
+
+my $cards = 'cards';
+$Store::database = database;
+$Store::FEE = 1;
+$Store::MAXSIZE = 1000 * 1024; #1000 kb
+$Store::USERAGENT = 'Mozilla/5.0 (Windows; U; Windows NT 5.0; it-IT; rv:1.7.12) Gecko/20050915';
+$Store::HOST = 'http://magiccards.info';
+$Store::CARDS = "public/$cards";
 
 get '/' => sub {
     template 'index' => {'title' => 'mtg'};
